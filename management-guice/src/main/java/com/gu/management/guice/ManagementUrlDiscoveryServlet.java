55ec0d4683bba4f5efd4c1b10be1e40c153eec6c
Graham Tackley <graham.tackley@guardian.co.uk>
2010-07-08 15:19:22 +0100

    Make management-guice url discovery servlet work in servlet 2.4
    It used to make use of a servlet 2.5 only method in ServletContext.
    One of our application servers only supports 2.4, so work around
    this dependency.
    
    I also deleted the (needless) intellij project files.

--- management-guice/src/main/java/com/gu/management/guice/ManagementUrlDiscoveryServlet.java
+++ management-guice/src/main/java/com/gu/management/guice/ManagementUrlDiscoveryServlet.java
@@ -20,7 +20,6 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.google.inject.servlet.ServletUrlPatternsDiscoveryService;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -32,12 +31,10 @@ import java.util.List;
 @Singleton
 public class ManagementUrlDiscoveryServlet extends HttpServlet {
 
-    private ServletContext servletContext;
     private ServletUrlPatternsDiscoveryService urlPatternDiscoveryService;
 
     @Inject
-    public ManagementUrlDiscoveryServlet(ServletContext servletContext, ServletUrlPatternsDiscoveryService urlPatternDiscoveryService) {
-        this.servletContext = servletContext;
+    public ManagementUrlDiscoveryServlet(ServletUrlPatternsDiscoveryService urlPatternDiscoveryService) {
         this.urlPatternDiscoveryService = urlPatternDiscoveryService;
     }
 
@@ -49,14 +46,11 @@ public class ManagementUrlDiscoveryServlet extends HttpServlet {
         PrintWriter writer = response.getWriter();
         writer.write("<html><head><title>Management URLs</title></head><body><h2>Management URLs</h2><ul>\n");
 
-
-        String contextPath = servletContext.getContextPath();
-
         List<String> urlPatterns = urlPatternDiscoveryService.getServletsUrlPatterns();
         for (String urlPattern : urlPatterns) {
             if (urlPattern.startsWith("/management/")) {
-                String path = contextPath + urlPattern;
-                writer.printf("<li><a href=\"%s\">%s</a></li>\n", path, urlPattern.replace("/management", ""));
+                String relativePath = urlPattern.substring(1);
+                writer.printf("<li><a href=\"%s\">%s</a></li>\n", relativePath, urlPattern.replace("/management", ""));
             }
         }
 
