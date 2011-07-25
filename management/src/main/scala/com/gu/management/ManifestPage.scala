84c9bfb7669da1fe39f7a0ed86295abbf8607918
Robbie Clutton <robbie.clutton@guardian.co.uk>
2011-07-25 15:45:43 +0100

    expose the manifest key/value pairs

--- management/src/main/scala/com/gu/management/ManifestPage.scala
+++ management/src/main/scala/com/gu/management/ManifestPage.scala
@@ -10,10 +10,27 @@ class ManifestPage extends ManagementPage {
   def get(req: HttpServletRequest) = response
 
   lazy val response = PlainTextResponse(
-    Option(getClass.getResourceAsStream("/version.txt"))
-            .map(Source.fromInputStream(_))
-            .map(_.mkString)
-            .getOrElse("Could not find version.txt on classpath.  Did you include the sbt-version-info-plugin?")
+    Manifest.asStringOpt.getOrElse("Could not find version.txt on classpath.  Did you include the sbt-version-info-plugin?")
   )
+
+}
+
+object Manifest {
+
+  val asStreamOpt = Option(getClass.getResourceAsStream("/version.txt")).map(Source.fromInputStream(_))
+  val asStringOpt = asStreamOpt.map(_.mkString)
+  val asList = asStringOpt match {
+    case Some(manifest) => {
+      manifest.split("\n").toList
+    }
+    case _ => List(): List[String]
+  }
+
+  lazy val asKeyValuePairs = {
+    asList.map(line => {
+      val tokens = line.split(":")
+      (tokens(0).trim, tokens(1).trim)
+    }).toMap
+  }
 
 }
