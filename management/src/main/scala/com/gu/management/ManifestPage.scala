d20e0e087c3ffb4e6e1b3340c408437280e82629
Robbie Clutton <robbie.clutton@guardian.co.uk>
2011-10-03 13:19:55 +0100

    expose manifest as a string

--- management/src/main/scala/com/gu/management/ManifestPage.scala
+++ management/src/main/scala/com/gu/management/ManifestPage.scala
@@ -19,6 +19,7 @@ object Manifest {
 
   lazy val asStreamOpt = Option(getClass.getResourceAsStream("/version.txt")).map(Source.fromInputStream(_))
   lazy val asStringOpt = asStreamOpt.map(_.mkString)
+  lazy val asString = asStringOpt getOrElse ""
   lazy val asList = asStringOpt map { _.split("\n").toList } getOrElse Nil
   lazy val asKeyValuePairs = (asList map { _.split(":") } collect { case Array(k, v) => k -> v}).toMap
 
