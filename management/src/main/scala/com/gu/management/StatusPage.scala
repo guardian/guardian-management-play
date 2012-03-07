9427ef22303f7ea843e6b40af7d4d5add153aad3
Ken Lim <kenneth.lim@guardian.co.uk>
2012-03-07 14:29:54 +0000

    New mutableStatusPage can now report on mutable list of metrics.

--- management/src/main/scala/com/gu/management/StatusPage.scala
+++ management/src/main/scala/com/gu/management/StatusPage.scala
@@ -2,9 +2,19 @@ package com.gu.management
 
 object StatusPage {
   def apply(application: String, metrics: Seq[Metric]) = new StatusPage(application, metrics)
+
 }
 
 class StatusPage(application: String, metrics: Seq[Metric]) extends JsonManagementPage {
   val path = "/management/status"
   def jsonObj = StatusResponseJson(application = application, metrics = metrics.map(_.asJson))
+}
+
+object MutableStatusPage {
+  def apply(application: String, metrics: () => Seq[Metric]) = new MutableStatusPage(application, metrics)
+}
+
+class MutableStatusPage(application: String, metrics: () => Seq[Metric]) extends JsonManagementPage {
+  val path = "/management/status"
+  def jsonObj = StatusResponseJson(application = application, metrics = metrics().map(_.asJson))
 }
