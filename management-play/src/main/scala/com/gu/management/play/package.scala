989bc6223aba2d6590b84fbca61567263dbd81a5
Simon Hildrew <simon.hildrew@guardian.co.uk>
2012-05-24 16:51:44 +0100

    Add plugin tests and refactor Management discovery

--- management-play/src/main/scala/com/gu/management/play/package.scala
+++ management-play/src/main/scala/com/gu/management/play/package.scala
@@ -1,7 +1,10 @@
 package com.gu.management.play
 
+import play.api.Application
 import play.api.mvc.{ AnyContent, Request }
 import com.gu.management.ListMultiMaps
+import org.reflections.Reflections
+import scala.collection.JavaConversions._
 
 object `package` extends ListMultiMaps {
 
@@ -20,4 +23,18 @@ object `package` extends ListMultiMaps {
   implicit def request2RequestURI[A](request: Request[A]) = new {
     lazy val requestURI: String = request.uri.replaceAll("\\?.*", "")
   }
+
+  implicit def application2GetConfigurationProperty(app: Application) = new {
+    def getConfigurationProperty(key: String, default: String): String = {
+      app.configuration.getString(key).getOrElse(default)
+    }
+  }
+
+  implicit def class2SubTypesFrom[T](supertype: Class[T]) = new {
+    def subTypesFrom(root: String): Set[Class[_ <: T]] = {
+      val reflections = new Reflections(root)
+      reflections.getSubTypesOf(supertype).toSet
+    }
+  }
+
 }
