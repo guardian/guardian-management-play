443466056756c3f67c1aa470e1791cbe002549cb
Daithi O Crualaoich <daithi.ocrualaoich@guardian.co.uk>
2012-08-10 12:29:37 +0100

    Don't publish the root project or examples.

--- project/ManagementBuild.scala
+++ project/ManagementBuild.scala
@@ -1,9 +1,16 @@
 import com.typesafe.sbtscalariform.ScalariformPlugin
 import org.sbtidea.SbtIdeaPlugin._
 import sbt._
+import sbt.Keys._
 import sbt.PlayProject._
 
 object ManagementBuild extends Build {
+
+  implicit def project2noPublish(project: Project) = new {
+    lazy val noPublish : sbt.Project = project.settings(publish := false)
+  }
+
+
 
   lazy val root = Project("management-root", file(".")).aggregate(
     management,
@@ -14,7 +21,7 @@ object ManagementBuild extends Build {
     managementMongo,
     exampleServletApi,
     examplePlay
-  ).settings(ideaSettings: _*)
+  ).settings(ideaSettings: _*).noPublish
 
   lazy val management = managementProject("management")
 
@@ -24,18 +31,21 @@ object ManagementBuild extends Build {
   lazy val managementLogback = managementProject("management-logback") dependsOn (management)
   lazy val managementMongo = managementProject("management-mongo") dependsOn  (management)
 
-  lazy val exampleServletApi = managementProject("example-servlet-api") dependsOn (
+  lazy val exampleServletApi = managementProject("example-servlet-api").dependsOn(
     management,
     managementServletApi,
     managementLogback
-  )
+  ).noPublish
 
   lazy val examplePlay = PlayProject(
     name = "example-play",
     applicationVersion = "1.0",
     dependencies = Nil,
     path = file("example-play"),
-    mainLang = SCALA).settings(ideaSettings: _*) dependsOn (management, managementPlay, managementLogback)
+    mainLang = SCALA).
+    settings(ideaSettings: _*).
+    dependsOn(management, managementPlay, managementLogback).
+    noPublish
 
   def managementProject(name: String) = Project(name, file(name)).settings(ideaSettings: _*).settings(ScalariformPlugin.scalariformSettings :_*)
 }
